ca4e2cb5bc01be2e58fe36206a8c668f07e68f42
Gordonmajoni <gordonmajoni95@gmail.com>
2022-07-16 17:09:33 +0200

    corrections

--- main/script.js
+++ main/script.js
@@ -1,3 +1,21 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+
+
+
+
+
+
 function myFunction() {
   var x = document.getElementById("click");
   if (x.className.indexOf("temple-show") == -1) { 
